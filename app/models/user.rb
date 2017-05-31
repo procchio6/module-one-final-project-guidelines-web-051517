e833a299e7662bcffe0a70560b1a45277de6b6b2
Pat Rocchio <procchio6@gmail.com>
2017-05-31 10:02:05 -0400

    Add correct_password? class method to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ActiveRecord::Base
   validates :username, :password, presence: true
   validates :username, uniqueness: true
 
+  def self.correct_password?(username, password)
+    user = User.find_by ({username:username})
+    if user
+      user.password == password ? user : false
+    end
+  end
 end
