4c82551bfa7dd5c3a962a269d0313de8a5fae706
Pat Rocchio <procchio6@gmail.com>
2017-05-31 08:52:29 -0400

    Add presence and uniqueness validations on User username and password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   has_many :rides
   has_many :drivers, through: :rides
 
+  validates :username, :password, presence: true
+  validates :username, uniqueness: true
+
 end
