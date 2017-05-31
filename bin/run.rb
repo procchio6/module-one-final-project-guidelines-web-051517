d76d9e7fc01d4d9deba4efe08fb098cd2408cf20
Zach Dubno <zach@Zach-Dubnos-MacBook-Pro.local>
2017-05-31 10:01:37 -0400

    created cli_methods and worked on runner method

--- bin/run.rb
+++ bin/run.rb
@@ -1,2 +1,13 @@
 require_relative 'config/environment'
 
+def run
+  case greet
+  when 1
+    current_user = verify_user
+  when 2
+    current_user = create_user
+  else
+    puts "Sorry, invalid input"
+    run
+  end
+end
