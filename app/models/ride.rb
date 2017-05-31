eff77d4605651b8c80cbb83fb4176aaf8ae2d2b3
Pat Rocchio <procchio6@gmail.com>
2017-05-31 09:09:30 -0400

    Remove attr_accessors from User and Ride models.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,6 +1,4 @@
 class Ride < ActiveRecord::Base
-  attr_accessor :fare, :date
-
   belongs_to :user
   belongs_to :driver
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  attr_accessor :username, :password
-
   has_many :rides
   has_many :drivers, through: :rides
 
