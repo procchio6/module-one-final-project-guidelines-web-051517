61c56d5eff5cc4156c5d29d8ef3e316084d3df4e
Pat Rocchio <procchio6@gmail.com>
2017-05-31 10:52:44 -0400

    Add commented out line in env file to hide SQL logging

--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,7 @@ require 'bundler'
 Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
+#Line below removes logging when uncommented
+#ActiveRecord::Base.logger = nil
 require_all 'lib'
 require_all 'app'
